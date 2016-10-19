3e41f4dc784c01fd1bc5a011e6232974ff9b8d10
zaslavskayasya <web.zaslavskaya@gmail.com>
2016-10-19 23:49:20 +0300

    committ

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+/**
+ * Created by User on 18.10.2016.
+ */
+module.exports = function(grunt) {
+
+    // project configuration
+    grunt.initConfig({
+        sass: {
+            options: {
+                //sourceMap : true
+            },
+            dist: {
+                files: {
+                    'style.css': 'style.scss'
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['sass', 'watch']);
+};
